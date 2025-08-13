e7461ee42b02890d85a350ef32aa69e53f217788
muslimbek77 <muslimbekbaratov57@gmail.com>
2025-08-13 10:11:54 +0500

    add contact

--- steve/views.py
+++ steve/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,FormView
+from .forms import ContactForm
+from django.urls import reverse_lazy
 
 class HomePageView(TemplateView):
     template_name = "index.html"
@@ -7,3 +9,12 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = "about-us.html"
+
+class ContactView(FormView):
+    template_name = "contact.html"
+    form_class = ContactForm
+    success_url = reverse_lazy('home-page')
+
+    def form_valid(self, form):
+        form.save() 
+        return super().form_valid(form)
